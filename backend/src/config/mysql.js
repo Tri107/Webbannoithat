c77b5b0ca0ede7e51b8533dca5a5f88302db097c
Nguyễn Minh Trí - 3368 <minhtri1072004@gmail.com>
2026-04-05 09:01:42 +0700

    cau hinh db tren cloud  de thuc hien deploy

--- backend/src/config/mysql.js
+++ backend/src/config/mysql.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
